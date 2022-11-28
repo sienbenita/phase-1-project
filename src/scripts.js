41ab1ed22a806be32998cd1dc67007dc7c8727bd
benita sien <benitasien@gmail.com>
2022-11-28 23:57:41 +1100

    implement loadedProducts array

--- src/scripts.js
+++ src/scripts.js
@@ -2,10 +2,17 @@
 const allProductTypes = ["blush", "bronzer", "eyebrow", "eyeliner", "eyeshadow", "foundation", "lip_liner", "lipstick", "mascara", "nail_polish"]
 const placeholderProductValues = {
     brand: "Uncle Joe's",
-    image: "./images/cosmetics.png",
+    image_link: "./images/cosmetics.png",
     name: "Freshest",
     price: "0.00"
 }
+
+// array of objects: 
+// {
+//     card: element, 
+//     product: object
+// }
+const loadedProducts = [];
 
 // query selectors
 const divProductContainer = document.querySelector("#product-container");
@@ -106,17 +113,35 @@ function populateProductContainer(products) {
     // return: none
 
     // remove all cards on page
+    loadedProducts = [];
     removeAllChildElements(divProductContainer);
 
     // create and append cards
     products.forEach(product => {
         const card = createProductCard(product.image_link, product.brand, product.name, product.price);
         divProductContainer.appendChild(card);
-        addProductImageErrorListenerToLast(placeholderProductValues.image);
+        addProductImageErrorListenerToLast(placeholderProductValues.image_link);
+        addToLoadedProducts(card, product);
     })
 }
 
+function addToLoadedProducts(card, product) {
+    // card: element, product: object
+    // create an object containing the card and product and push it to the loadedObjects array
+    // return: none
+    
+    const productObject = {
+        "card": card,
+        "product": product
+    }
+    loadedProducts.push(productObject);
+}
+
 function addProductImageErrorListenerToLast(placeholder) {
+    // placeholder: string image url
+    // add an event listener to the last product image
+    // if there is an error retrieving the image, use the placeholder image instead
+    // return: none
     const productImages = document.querySelectorAll(".product-image");
     const productImagesLast = productImages[productImages.length - 1];
     productImagesLast.addEventListener("error", () => productImagesLast.src = placeholder);
@@ -128,15 +153,18 @@ function createCheckBoxAndLabel(value, labelText) {
     
     value = value.toLowerCase()
 
+    // create checkbox
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
     checkbox.value = value;
     checkbox.name = value;
 
+    // create label
     const label = document.createElement("label");
     label.for = value;
     label.innerText = toTitleCase(labelText);
 
+    // create container and append checkbox + label
     const container = document.createElement("div");
     container.appendChild(checkbox);
     container.appendChild(label);
@@ -146,19 +174,32 @@ function createCheckBoxAndLabel(value, labelText) {
 }
 
 function populateFilterContainer(filterContainer, array, title) {
+    // filterContainer: element to which to append children, array: array of filter values
+    // populate the filter container with filter checkboxes and labels
+    // return: none
+
     removeAllChildElements(filterContainer);
+
     const h2 = document.createElement("h2");
     h2.textContent = toTitleCase(title);
     filterContainer.appendChild(h2);
+
+    // create and append filter checkboxes and labels
     array.forEach(e => {
-        filterContainer.appendChild(createCheckBoxAndLabel(e, e))
+        filterContainer.appendChild(createCheckBoxAndLabel(e, e)) // the value and label text are the same
     })
 }
 
 function removeAllChildElements(parent) {
+    // parent: the element from which to remove all children
+    // remove all of the children of parent
+    // return: none
+
     let child = parent.lastElementChild;
     while (child) {
         parent.removeChild(child);
         child = parent.lastElementChild;
     }
 }
+
+populateProductContainer([placeholderProductValues, placeholderProductValues])
